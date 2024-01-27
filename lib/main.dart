bd14f216c37e0f475b18d6bed725e46ada4da639
Prihantoedi <prihanto_sanjaya@yahoo.com>
2024-01-27 09:26:45 +0700

    remove the home in material app

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:world_time/pages/loading.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Home(),
     routes: {
       '/' : (context) => Loading(),
       '/home': (context) => Home(),
