33b0b9d370c7e849e234b1eb7905f709b8a8473d
Prihantoedi <prihanto_sanjaya@yahoo.com>
2024-01-27 09:40:02 +0700

    modify the page of home and choose_location

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:world_time/pages/loading.dart';
 
 void main() {
   runApp(MaterialApp(
+    initialRoute: '/home',
     routes: {
       '/' : (context) => Loading(),
       '/home': (context) => Home(),
--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -11,6 +11,19 @@ class _ChooseLocationState extends State<ChooseLocation> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        iconTheme: IconThemeData(color: Colors.white), // change the color of back button in appbar
+        title: Text(
+            'Choose a Location',
+            style: TextStyle(
+              color: Colors.white
+            ),
+        ),
+        centerTitle: true,
+        elevation: 0,
+      ),
       body: Text('choose location screen'),
     );
   }
