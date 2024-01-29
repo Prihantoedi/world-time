4ffd17b74ed366f07832ca9cc54629d595133965
Prihantoedi <prihanto_sanjaya@yahoo.com>
2024-01-29 16:15:34 +0700

    add the loading spinner in loading.dart

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:world_time/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 
 class Loading extends StatefulWidget {
@@ -15,7 +16,6 @@ class _LoadingState extends State<Loading> {
   void setupWorldTime() async{
     WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
     await instance.getTime();
-    // print(instance.time);
     Navigator.pushReplacementNamed(context, '/home', arguments: {
       'location' : instance.location,
       'flag' : instance.flag,
@@ -32,10 +32,13 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-          padding: EdgeInsets.all(50.0),
-          child: Text('loading'),
-      ),
+      backgroundColor: Colors.blue[900],
+      body: Center(
+        child: SpinKitFadingCube(
+          color: Colors.white,
+          size: 80.0,
+        ),
+      )
     );
   }
 }
--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -38,8 +38,6 @@ class WorldTime{
         time = 'could not get time data';
     }
 
-
-
   }
 
 }
