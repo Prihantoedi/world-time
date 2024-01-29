0bb1c14bafd70e05610b8d72531faa931d8c8dc9
Prihantoedi <prihanto_sanjaya@yahoo.com>
2024-01-29 19:16:21 +0700

    add the list view to show the location of time in choose_location.dart

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:world_time/services/world_time.dart';
 
 class ChooseLocation extends StatefulWidget {
   const ChooseLocation({super.key});
@@ -8,7 +9,16 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
-
+  List<WorldTime> locations = [
+    WorldTime(location: 'London', flag: 'uk.png', url: 'Europe/London'),
+    WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin'),
+    WorldTime(location: 'Cairo', flag: 'egypt.png', url: 'Africa/Cairo'),
+    WorldTime(location: 'Nairobi', flag: 'kenya.png', url: 'Africa/Nairobi'),
+    WorldTime(location: 'Chicago', flag: 'usa.png', url: 'America/Chicago'),
+    WorldTime(location: 'New York', flag: 'usa.png', url: 'America/New_York'),
+    WorldTime(location: 'Seoul', flag: 'south_korea.png', url: 'Asia/Seoul'),
+    WorldTime(location: 'Jakarta', flag: 'indonesia.png', url: 'Asia/Jakarta')
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -26,9 +36,24 @@ class _ChooseLocationState extends State<ChooseLocation> {
         centerTitle: true,
         elevation: 0,
       ),
-      body: ElevatedButton(
-          onPressed: (){},
-          child: Text('counter is'),
+      body: ListView.builder(
+          itemCount: locations.length,
+          itemBuilder: (context, index){
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 1.0, horizontal: 4.0),
+              child: Card(
+                child: ListTile(
+                  onTap: (){
+                    print(locations[index].location);
+                  },
+                  title: Text(locations[index].location as String),
+                  leading: CircleAvatar(
+                    backgroundImage: AssetImage('assets/${locations[index].flag}'),
+                  ),
+                ),
+              ),
+            );
+          },
       )
     );
   }
